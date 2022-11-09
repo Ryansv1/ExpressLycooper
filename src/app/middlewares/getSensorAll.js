3a7642c85b2a8a295fdac7f9375943b045c41d7b
Ryansv1 <ryaan.gs2020@Gmail.com>
2022-11-09 20:18:50 -0300

    forgot pass

--- src/app/middlewares/getSensorAll.js
+++ src/app/middlewares/getSensorAll.js
@@ -3,8 +3,11 @@ const Sensores = require('../../models/sensores')
 module.exports = async (req, res, next) =>{
     const { sensor, dataColeta } = req.query
     try {
+        if(!dataColeta) return res.redirect('/consulta?err=1')
+        if(!sensor) return res.redirect('/consulta?err=1')
         if(!sensor || !dataColeta) return res.redirect('/consulta?err=1')
-        req.resultados = await Sensores.find({id:sensor, createdAt: dataColeta});  
+        req.resultados = await Sensores.find({id:sensor, createdAt: dataColeta});
+        if (!req.resultados[0]) return res.redirect('/consulta?err=2') 
         return next();
     } catch (err) {
         console.log(err)
